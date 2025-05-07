01fab80cdc46ae256706a6db3998681bfad62071
woooojeong <jwj961016@naver.com>
2025-05-07 13:43:15 +0900

    환경설정

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+// postcss.config.js
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  plugins: [tailwindcss, autoprefixer],
+};
--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { DiAtom } from "react-icons/di";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='bg-slate-500'>
           Edit <code>src/App.js</code> and save to reload.
+          <DiAtom />
         </p>
         <a
           className="App-link"
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <h1 className="text-3xl font-bold underline text-blue-500">Tailwind 적용 완료!</h1>
       </header>
     </div>
   );
