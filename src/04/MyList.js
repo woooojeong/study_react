2e4688644b3d153c29ce97a5c57dd46bf5cb4584
woooojeong <jwj961016@naver.com>
2025-05-09 10:25:44 +0900

    unique "key" prop 오류해결 및 useState Hook 추가가

--- src/04/MyList.js
+++ src/04/MyList.js
@@ -4,7 +4,7 @@ import MyListItem from './MyListItem';
 export default function MyList() {
     //console.log(MyListData)
     const tags = MyListData.map(item => 
-                                <MyListItem
+                                <MyListItem key = {item.title}
                                             title = {item.title}
                                             imgUrl = {item.imgUrl}
                                             content = {item.content}
@@ -12,7 +12,7 @@ export default function MyList() {
                             ); //map메소드를 사용하여 아이템의 개수만큼 만들어진 데이터를 tags 변수에 담는다.
 
   return (
-    <div className='w-full grid grid-cols-2 gap-4'>
+    <div className='w-10/12 grid grid-cols-2 gap-4'>
       {/* <MyListItem/>
       <MyListItem/>
       <MyListItem/>
--- src/04/MyListItem.js
+++ src/04/MyListItem.js
@@ -1,6 +1,17 @@
-
+import { useState } from "react";   //React에서 useState를 import 한다.
 
 export default function MyListItem({title, imgUrl, content}) {
+    // 증가건수 변수선언 -> state 변수로 변경
+    // let cnt = 0;
+    const [cnt,setCnt] = useState(0);
+
+    // 클릭이벤트 함수선언
+    const handleClick = () => {
+        // cnt = cnt + 1 ;
+        //변경된 상태값(setCnt)이 나오도록한다.
+        setCnt( cnt + 1 ) ;
+        console.log('cnt=', cnt);   //state로 변경 후 여기에 로그를 찍으면 이전 값이 나오게된다.
+    }
   return (
     <div className="flex w-full h-full justify-center items-start p-2 border border-slate-400">
       <div className="flex w-1/3 m-2">
@@ -12,9 +23,9 @@ export default function MyListItem({title, imgUrl, content}) {
             <p>{content}</p>
         </div>
         <div className="flex justify-end items-center">
-            <span>❤️</span>
+            <span onClick={handleClick} className="cursor-pointer">❤️</span>
             <span className="inline-flex mx-2 font-bold">좋아요</span>
-            <span className="font-bold text-xl">0</span>
+            <span className="font-bold text-xl">{cnt}</span>
         </div>
       </div>
     </div>
