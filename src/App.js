7a8ccd339177fd805b3fb4f2073ae2cd2d3bd714
woooojeong <jwj961016@naver.com>
2025-05-15 10:21:51 +0900

    App.js에 브라우저라우터를 설정하여 링크를 통해 실습한 내용 메뉴화하기

--- src/App.js
+++ src/App.js
@@ -2,55 +2,74 @@
 import './App.css';
 // import { DiAtom } from "react-icons/di";  // 아이콘도 마찬가지.
 // import Hello from './01/Hello';
-// import MyClock from './02/MyClock';
+import MyClock from './02/MyClock';
 // import MyDiv1 from './03/MyDiv1';
 // import MyList from './04/MyList';
-// import Lotto from './05/Lotto';
-// import Traffic from './06/Traffic';
+import Lotto from './05/Lotto';
+import Traffic from './06/Traffic';
 // import MyRef from './07/MyRef';
-// import MyRefAdd from './07/MyRefAdd';
-// import Gallery from './08/Gallery';
-import RouteMain from './09/RouteMain';
-
+import MyRefAdd from './07/MyRefAdd';
+import Gallery from './08/Gallery';
+// import RouteMain from './09/RouteMain';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { RiHomeHeartFill } from "react-icons/ri";
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <MyClock />
-    //     {/* <img src={logo} className="App-logo" alt="logo" /> */}
-    //     {/* <Hello /> */}
-    //     {/* <p className='bg-slate-500'>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //       <DiAtom />
-    //     </p> */}
-       
-    //   </header>
-    // </div>
-
-    // TailwindCSS를 이용한 화면재구성
-    <div className="flex flex-col w-full h-screen mx-auto">
-      <header className='flex justify-between items-center text-xl font-bold h-20 p-10 bg-slate-200'>
-        <p>React Basic</p>
-        <p><RiHomeHeartFill/></p>
-      </header>
-      {/* grow : header와 footer를 쓰고 남은 영역을 모두 사용 */}
-      <main className='grow w-full flex justify-center items-center overflow-y-auto'>
-        {/* <MyClock /> */}
-        {/* <MyDiv1/> */}
-        {/* <MyList/> */}
-        {/* <Lotto /> */}
-        {/* {<Traffic />} */}
-        {/* <MyRef /> */}
-        {/* <MyRefAdd /> */}
-        {/* <Gallery /> */}
-        <RouteMain />
-      </main>
-      <footer className='flex justify-center items-center h-20 bg-black text-slate-100'>
-        ⓒ Jeong Woo Jeong
-      </footer>
-    </div>
+    <BrowserRouter>
+      <div className="flex flex-col w-full h-screen mx-auto">
+        <header className='flex justify-between items-center text-xl font-bold h-20 p-10 bg-slate-200'>
+          <p>React Basic</p>
+          <ul className="flex justify-center items-center text-sm">
+            <li className="mx-2 p-2 rounded-r-md
+                          hover:bg-white hover:text-blue-600">
+              <Link to='/'>시계</Link>
+            </li>
+            <li className="mx-2 p-2 rounded-r-md
+                          hover:bg-white hover:text-blue-600">
+              <Link to='/lotto'>로또</Link>
+            </li>
+            <li className="mx-2 p-2 rounded-r-md
+                          hover:bg-white hover:text-blue-600">
+              <Link to='/traffic'>교통사고</Link>
+            </li>
+            <li className="mx-2 p-2 rounded-r-md
+                          hover:bg-white hover:text-blue-600">
+              <Link to='/add'>더하기연산</Link>
+            </li>
+            <li className="mx-2 p-2 rounded-r-md
+                          hover:bg-white hover:text-blue-600">
+              <Link to='/gallery'>관광앱</Link>
+            </li>
+          </ul>
+          <p><Link to='/'><RiHomeHeartFill/></Link></p>
+        </header>
+        {/* grow : header와 footer를 쓰고 남은 영역을 모두 사용 */}
+        <main className='grow w-full flex justify-center items-start overflow-y-auto'>
+          <Routes>
+             <Route path='/' element={<MyClock />}/>
+             <Route path='/lotto' element={<Lotto />}/>
+             <Route path='/traffic' element={<Traffic />}/>
+             <Route path='/add' element={<MyRefAdd />}/>
+             <Route path='/gallery' element={<Gallery />}/>
+            <Route />
+          </Routes>
+          {/* <MyClock /> */}
+          {/* <MyDiv1/> */}
+          {/* <MyList/> */}
+          {/* <Lotto /> */}
+          {/* {<Traffic />} */}
+          {/* <MyRef /> */}
+          {/* <MyRefAdd /> */}
+          {/* <Gallery /> */}
+          {/* <RouteMain /> */}
+        </main>
+        <footer className='flex justify-center items-center h-20 bg-black text-slate-100'>
+          ⓒ Jeong Woo Jeong
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
